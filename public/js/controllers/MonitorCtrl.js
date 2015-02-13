61b47d03bebd81271e9f3780bd461dc2fd367db8
sjbgit <sjb_strat@hotmail.com>
2015-02-12 18:13:12 -0600

    more monitor

--- public/js/controllers/MonitorCtrl.js
+++ public/js/controllers/MonitorCtrl.js
@@ -1,8 +1,34 @@
 /**
  * Created by sbunke on 2/12/2015.
  */
-angular.module('MonitorCtrl', ['MonitorService']).controller('MonitorController', function($scope) {
+angular.module('MonitorCtrl', ['MonitorService']).controller('MonitorController', ['$scope', 'Monitor', function ($scope, Monitor) {
 
     $scope.tagline = 'This is the monitor page';
 
-});
+    var mon = Monitor;
+
+    mon.getCount()
+        .success(function (data) {
+            $scope.count = data[0].count;
+
+            console.log(data);
+
+        })
+        .error(function (error) {
+            $scope.status = 'Unable to load customer data: ' + error.message;
+        });
+
+    mon.getTopLogs()
+        .success(function (data) {
+            $scope.logs = data;
+
+            console.log(data);
+
+        })
+        .error(function (error) {
+            $scope.status = 'Unable to load customer data: ' + error.message;
+        });
+
+    console.log(mon);
+
+}]);
--- public/js/services/MonitorService.js
+++ public/js/services/MonitorService.js
@@ -1,8 +1,51 @@
 /**
  * Created by sbunke on 2/12/2015.
  */
-angular.module('MonitorService', []).factory('Monitor', ['$http', function($http) {
+angular.module('MonitorService', ['ngResource'])
 
+    .factory('Monitor', ['$http', '$resource', function ($http, $resource) {
+        var urlBase = '/api/customers';
+        var dataFactory = {};
 
+        dataFactory.getCount = function() {
+          return $http.get('/api/monitor');
+        };
 
-}]);
+        dataFactory.getTopLogs = function () {
+            return $http.get('/api/monitor/toplogs');
+        };
+
+        dataFactory.getCustomers = function () {
+            return $http.get(urlBase);
+        };
+
+        dataFactory.getCustomer = function (id) {
+            return $http.get(urlBase + '/' + id);
+        };
+
+        dataFactory.insertCustomer = function (cust) {
+            return $http.post(urlBase, cust);
+        };
+
+        dataFactory.updateCustomer = function (cust) {
+            return $http.put(urlBase + '/' + cust.ID, cust)
+        };
+
+        dataFactory.deleteCustomer = function (id) {
+            return $http.delete(urlBase + '/' + id);
+        };
+
+        dataFactory.getOrders = function (id) {
+            return $http.get(urlBase + '/' + id + '/orders');
+        };
+
+        return dataFactory;
+
+    }])
+
+    .factory('Post', ['$resource', function ($http, $resource) {
+        return $resource('/api/post');
+
+    }])
+
+    ;
